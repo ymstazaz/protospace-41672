f5049f442ad4e3ea37d7db14dc8acb20e7d8b6e5
ymstazaz <r37a45d@gmail.com>
2024-10-24 23:00:00 +0900

    プロトタイプのアクションとルーティング
    ※インデックスについては要確認

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,9 @@ class PrototypesController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
 
-  resources :_
+  resources :prototypes, only: [:index, :new, :create]
   # get 'prototypes', to: 'prototypes#index'
 end
