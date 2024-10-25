73092f738cf77d67013ba3b9d0ecf20afbbf2684
ymstazaz <r37a45d@gmail.com>
2024-10-25 14:16:42 +0900

    プライベートメソッドでストロングパラメータを設定
     prototypesコントローラーのprivateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるようにした。且つ、user_idもmergeした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,4 +9,9 @@ class PrototypesController < ApplicationController
 
   def create
   end
+
+  private
+  def Prototype_params
+    params.permit(:image, :title, :catch_copy, :concept).marge(user_id: current_user.id)
+  end
 end
