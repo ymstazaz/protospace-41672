ee01c20e4d97548aa81112e4a13f408314946624
ymstazaz <r37a45d@gmail.com>
2024-10-28 14:09:45 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to root_path
     end
   end
